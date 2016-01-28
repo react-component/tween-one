9273fc62191d95dde9cbce3ea80507170bbcce8d
jljsj <liangjun.jiang@alipay.com>
2016-01-28 21:18:30 +0800

    update demo simple

--- examples/simple.js
+++ examples/simple.js
@@ -7,17 +7,12 @@ class Demo extends React.Component {
     super(...arguments);
   }
 
-  abc(t, e) {
-    // update里传回出当前的百分比；
-    console.log('bind:' + t, 'update:' + e);
-  }
-
   bbb(e) {
     console.log(e);
   }
 
   render() {
-    return (<Tween animation={{top: '500px'}}
+    return (<Tween animation={{translateX: '500px'}}
                    onChange={this.bbb.bind(this)}
                    style={{opacity: 1, height: 100, transform: 'translate(50px,30px)'}}>
       <div>执行动效</div>
