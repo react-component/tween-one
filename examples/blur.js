5e2a2a9827b05026542533ad8a04864cfd7d9dc7
jljsj <liangjun.jiang@alipay.com>
2016-05-10 16:59:20 +0800

    update examples blur

--- examples/blur.js
+++ examples/blur.js
@@ -9,7 +9,7 @@ class Demo extends React.Component {
 
   render() {
     return (<div>
-      <div>模糊与其它filter里滤镜，只支持xxx(10),可以多个</div>
+      <div>filter 里的滤镜，'grayScale', 'sepia', 'hueRotate', 'invert', 'brightness', 'contrast', 'blur'</div>
       <Tween animation={{blur: '10px', sepia: '100%', duration: 2000}} style={{ filter: 'blur(30px)'}}>
         <img width="500" src="https://t.alipayobjects.com/images/T1CFtgXb0jXXXXXXXX.jpg"/>
       </Tween>
