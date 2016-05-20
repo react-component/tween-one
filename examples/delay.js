687c0a74b7f19fdb0c4d77b1b6f2199a903791d8
jljsj <liangjun.jiang@alipay.com>
2016-05-20 14:57:15 +0800

    recovery demo

--- examples/delay.js
+++ examples/delay.js
@@ -8,7 +8,7 @@ class Demo extends React.Component {
   }
 
   render() {
-    return (<Tween animation={[{translateX: '50em', delay: 1000}, {delay: -350, y: 150}]}
+    return (<Tween animation={[{translateX: '500px', delay: 1000}, {delay: -350, y: 150}]}
                    style={{opacity: 1, height: 300, transform: 'translate(50px,30px)'}}>
       <div>执行动效</div>
     </Tween>);
