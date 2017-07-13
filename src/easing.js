c9eb4c57b8497e937eeff3a9409b0ae80e9bc55e
jljsj <liangjun.jiang@alipay.com>
2017-07-13 16:15:16 +0800

    fix https://github.com/react-component/tween-one/issues/18

--- src/easing.js
+++ src/easing.js
@@ -8,9 +8,10 @@ easingTypes.path = (_path, _param) => {
   const rect = param.rect || 100;// path 的大小，100 * 100，
   const lengthPixel = param.lengthPixel || 200; // 线上取点像素，默认分为 200 段。。
   const points = [];
-  for (let i = 0; i < lengthPixel; i++) {
-    points.push(pathNode.getPointAtLength((pathLength / lengthPixel) * i));
+  for (let i = 0; i < lengthPixel - 1; i++) {
+    points.push(pathNode.getPointAtLength((pathLength / (lengthPixel - 1)) * i));
   }
+  points.push(pathNode.getPointAtLength(lengthPixel));
   return function path(t, b, _c, d) {
     const p = easingTypes.linear(t, b, _c, d);
     const timePointX = rect * p; // X 轴的百分比;
