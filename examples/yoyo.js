4940b770d0dca08492869f1b4991e489839d4716
jljsj <liangjun.jiang@alipay.com>
2017-07-13 16:15:51 +0800

    recovery demo

---
+++ examples/yoyo.js
@@ -0,0 +1,21 @@
+import Tween from 'rc-tween-one';
+import React from 'react';
+import ReactDom from 'react-dom';
+class Demo extends React.Component {
+  constructor() {
+    super(...arguments);
+  }
+
+  render() {
+    return (<Tween
+      animation={[
+      { opacity: 1, duration: 1000 },
+      { x: 300, duration: 1000 },
+      { translateX: '100px', repeat: -1, duration: 1000, yoyo: true },
+      ]} style={{ opacity: 0 }}
+    >
+      <div>执行动效</div>
+    </Tween>);
+  }
+}
+ReactDom.render(<Demo />, document.getElementById('__react-content'));
