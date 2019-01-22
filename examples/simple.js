3ccc89bf6282fcddc32c55ce07ae776a4f6bdbb1
jljsj33 <liangjun.jiang@alipay.com>
2019-01-22 17:10:59 +0800

    re simple

--- examples/simple.js
+++ examples/simple.js
@@ -10,9 +10,9 @@ class Demo extends React.Component {
 
   render() {
     return (<Tween
-      animation={{ x: 300}}
-      style={{ opacity: 1, height: 100, display: 'inline-block' }}
+      animation={{ x: 300 }}
       onChange={this.bbb}
+      style={{ opacity: 1, height: 100, display: 'inline-block' }}
     >
       <div>执行动效</div>
     </Tween>);
