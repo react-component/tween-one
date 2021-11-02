8b4f508cde0009ab67975711927fea890bf149fc
jljsj <liangjun.jiang@alipay.com>
2021-11-02 21:38:10 +0800

    use tree shaking, add sideEffects

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import { Ticker, Plugins, Easing } from 'tween-one';
 import TweenOne from './TweenOne';
+import TweenOneGroup from './TweenOneGroup';
+import ChildrenPlugin from './plugin/ChildrenPlugin';
+import PathMotionPlugin from './plugin/PathMotionPlugin';
+import SvgDrawPlugin from './plugin/SvgDrawPlugin';
+import SvgMorphPlugin from './plugin/SvgMorphPlugin';
 
 export * from './type';
 
-export { Easing, Ticker, Plugins };
+export {
+  Easing,
+  Ticker,
+  Plugins,
+  TweenOneGroup,
+  ChildrenPlugin,
+  PathMotionPlugin,
+  SvgDrawPlugin,
+  SvgMorphPlugin,
+};
 
 TweenOne.plugins = Plugins;
 TweenOne.ticker = Ticker;
