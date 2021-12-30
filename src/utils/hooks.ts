ddec22cb62b0404418ad81f42cca3f60e71dd250
TongZ <471205975@qq.com>
2021-12-30 22:34:11 +0800

    enhance: create useIsomorphicLayoutEffect

---
+++ src/utils/hooks.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+    ? React.useLayoutEffect
+    : React.useEffect
